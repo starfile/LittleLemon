01d7a394a2c72ee9f9ccda8cbc5df8d97b78aa20
starfile <bk@bluewin.ch>
2023-12-02 08:44:21 +0100

    Static Content

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def sayHello(request):
+ return HttpResponse('Hello World')
+
+def index(request):
+    return render(request, 'index.html', {})
