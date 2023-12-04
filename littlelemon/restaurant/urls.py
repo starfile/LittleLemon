a27bc93b64905d4581516ed6cc22e75b088c4156
starfile <bk@bluewin.ch>
2023-12-04 07:40:43 +0100

    User Authentication

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path 
 #from .views import sayHello 
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
   
 urlpatterns = [ 
 #    path('', sayHello, name='sayHello'),
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from rest_framework import generics, viewsets
 from .models import Booking, Menu
 from .serializers import BookingSerializer, MenuSerializer
+from rest_framework.permissions import IsAuthenticated
 
 def sayHello(request):
  return HttpResponse('Hello World')
@@ -13,6 +14,7 @@ def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -23,3 +25,4 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
